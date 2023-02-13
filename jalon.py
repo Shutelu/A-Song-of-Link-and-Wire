158bb50c8c25700ea2bb9c03e2af5c020919f733
Shutelu <changkai.wang@outlook.com>
2023-02-13 16:34:47 +0100

    add function svg_dico

--- jalon.py
+++ jalon.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 #Question 1:
-def list_liens(page):
+def liste_liens(page) -> list:
 
     adresse = requests.get("https://iceandfire.fandom.com/wiki/" + page)
     soup = BeautifulSoup(adresse.text, 'html.parser')
@@ -17,9 +17,24 @@ def list_liens(page):
         if not a.get('class') or (a.get('class')[0] not in ['image','image-thumbnail']):
             #on veut pas les ancres cad les liens '#'
             if not a.get('href').startswith('#'):
-                print(a.get('href'))
+                # print(a.get('href'))
                 links_from_div.append(a.get('href'))
     
     return links_from_div
 
-# list_liens("Petyr_Baelish") #test
+#Question 2:
+def svg_dico(dico, fichier) -> None:
+    #auto close syntax
+    with open(fichier, "a") as file:
+        #prendre l'indice 0 de la list des clef et valeurs
+        file.write("{}:{}\n".format(list(dico.keys())[0], list(dico.values())[0]))
+    # print(list(dico.keys())[0])
+
+    
+#test Q2
+# svg_dico({
+#     "Petyr_Baelish": liste_liens("Petyr_Baelish")
+# },"f1.txt")
+
+#test Q1
+# liste_liens("Petyr_Baelish") #test
