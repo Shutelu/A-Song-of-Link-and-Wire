46210a37b3b2eefaeca74b8010117d7c0db33770
Shutelu <changkai.wang@outlook.com>
2023-02-15 20:53:33 +0100

    modif new syntax

--- jalon.py
+++ jalon.py
@@ -8,33 +8,45 @@ def liste_liens(page) -> list:
     soup = BeautifulSoup(adresse.text, 'html.parser')
     links_from_div = []
 
-    #div principe ou on va chercher le main div
     main_div_to_research = soup.find('div', class_="mw-parser-output")
 
-    #on reccupere les a du main_div_to_research, abreviation main('a')
-    for a in main_div_to_research.find_all('a'):#find all return a list
+    #abreviation main('a')
+    for a in main_div_to_research.find_all('a'):#find_all return a list
+
+        #aide explication
+        # if a.get('class'):
+        #     print(a.get('class')[0])
+        #     if(len(a.get('class')) > 1):
+        #         print(a.get('class')[1])
         #on veut pas le liens de image d'affichage
-        if not a.get('class') or (a.get('class')[0] not in ['image','image-thumbnail']):
-            #on veut pas les ancres cad les liens '#'
+
+        class_a = a.get('class')
+        forbid = ['image','image-thumbnail']
+
+        # if not a.get('class') or (len(a.get('class')) > 0 and a.get('class')[0] not in ['image','image-thumbnail']):#old
+        if not class_a or len(class_a) > 0 and class_a[0] not in forbid or len(class_a) > 1 and class_a[1] not in forbid:#new
+
             if not a.get('href').startswith('#'):
-                # print(a.get('href'))
                 links_from_div.append(a.get('href'))
-    
+                # print(a.get('href'))
+
     return links_from_div
 
 #Question 2:
 def svg_dico(dico, fichier) -> None:
+
     #auto close syntax
     with open(fichier, "a") as file:
+
         #prendre l'indice 0 de la list des clef et valeurs
-        file.write("{}:{}\n".format(list(dico.keys())[0], list(dico.values())[0]))
-    # print(list(dico.keys())[0])
+        # file.write("{}:{}\n".format(list(dico.keys())[0], list(dico.values())[0]))#old
+        file.write(f"{list(dico.keys())[0]}:{list(dico.values())[0]}\n")#new
 
     
-#test Q2
-# svg_dico({
-#     "Petyr_Baelish": liste_liens("Petyr_Baelish")
-# },"f1.txt")
+# test Q2
+svg_dico({
+    "Petyr_Baelish": liste_liens("Petyr_Baelish")
+},"f1.txt")
 
 #test Q1
-# liste_liens("Petyr_Baelish") #test
+liste_liens("Petyr_Baelish") #test
