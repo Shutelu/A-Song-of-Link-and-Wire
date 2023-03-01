00ee517a1e93c16c69eaeecc3d9b55c09b7d84d5
Changkai <changkai.wang@outlook.com>
2023-03-01 15:48:39 +0100

    add q3 + small change on q2

--- jalon.py
+++ jalon.py
@@ -10,7 +10,7 @@ def liste_liens(page) -> list:
 
     main_div_to_research = soup.find('div', class_="mw-parser-output")
 
-    #abreviation main('a')
+    #test if we can add the link a in our list links_from_div
     for a in main_div_to_research.find_all('a'):#find_all return a list
 
         #aide explication
@@ -21,10 +21,11 @@ def liste_liens(page) -> list:
         #on veut pas le liens de image d'affichage
 
         class_a = a.get('class')
-        forbid = ['image','image-thumbnail']
+        forbidden_class = ['image','image-thumbnail']
 
+        #exclude forbidden class and alias
         # if not a.get('class') or (len(a.get('class')) > 0 and a.get('class')[0] not in ['image','image-thumbnail']):#old
-        if not class_a or len(class_a) > 0 and class_a[0] not in forbid or len(class_a) > 1 and class_a[1] not in forbid:#new
+        if not class_a or (len(class_a) > 0 and class_a[0] not in forbidden_class) or (len(class_a) > 1 and class_a[1] not in forbidden_class):#new
 
             if not a.get('href').startswith('#'):
                 links_from_div.append(a.get('href'))
@@ -33,20 +34,38 @@ def liste_liens(page) -> list:
     return links_from_div
 
 #Question 2:
-def svg_dico(dico, fichier) -> None:
+#save a dico into the file
+def svg_dico(dico, file) -> None:
 
     #auto close syntax
-    with open(fichier, "a") as file:
+    with open(file, "a") as f:
 
         #prendre l'indice 0 de la list des clef et valeurs
         # file.write("{}:{}\n".format(list(dico.keys())[0], list(dico.values())[0]))#old
-        file.write(f"{list(dico.keys())[0]}:{list(dico.values())[0]}\n")#new
+        if len(dico) > 0:
+            f.write(f"{list(dico.keys())[0]}:{list(dico.values())[0]}\n")
+        else:
+            f.write("Empty dictionary\n")
 
-    
-# test Q2
-svg_dico({
-    "Petyr_Baelish": liste_liens("Petyr_Baelish")
-},"f1.txt")
+#Question 3 
+#use the file to creation a new dico
+def chg_dico(file):
+    newdico = {}
+    with open(file, 'r') as f:
+        for line in f:
+            line = line.strip()#remove escape
+            if not line:
+                continue  # ignorer les lignes vides
+            key, value = line.split(':', 1)#split from : 1 time
+            newdico[key] = value
+    return newdico
 
 #test Q1
 liste_liens("Petyr_Baelish") #test
+# # test Q2
+# svg_dico({
+#     "Petyr_Baelish": liste_liens("Petyr_Baelish")
+# },"f1.txt")
+#test Q3
+testdico = chg_dico("f1.txt")
+print(testdico)
