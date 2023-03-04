f8cf6f7c85afe17c1dc9c95a2d0a855ad238dacc
Changkai <changkai.wang@outlook.com>
2023-03-04 13:24:42 +0100

    comment and small change in q1

--- jalon.py
+++ jalon.py
@@ -2,37 +2,29 @@ from bs4 import BeautifulSoup
 import requests
 
 #Question 1:
+#return all the links of the page in a list
 def liste_liens(page) -> list:
 
     adresse = requests.get("https://iceandfire.fandom.com/wiki/" + page)
     soup = BeautifulSoup(adresse.text, 'html.parser')
-    links_from_div = []
+    links_from_div = [] #list to return
 
+    #search the main information zone
     main_div_to_research = soup.find('div', class_="mw-parser-output")
 
     #test if we can add the link a in our list links_from_div
     for a in main_div_to_research.find_all('a'):#find_all return a list
 
-        #aide explication
-        # if a.get('class'):
-        #     print(a.get('class')[0])
-        #     if(len(a.get('class')) > 1):
-        #         print(a.get('class')[1])
-        #on veut pas le liens de image d'affichage
-
-        class_a = a.get('class')
+        a_class = a.get('class')
         forbidden_class = ['image','image-thumbnail']
 
-        #exclude forbidden class
-        # if not a.get('class') or (len(a.get('class')) > 0 and a.get('class')[0] not in ['image','image-thumbnail']):#old
-        if not class_a or (len(class_a) > 0 and class_a[0] not in forbidden_class) or (len(class_a) > 1 and class_a[1] not in forbidden_class):#new
+        #autorize link without class and exlude forbidden class
+        if (not a_class) or (len(a_class) > 0 and a_class[0] not in forbidden_class) or (len(a_class) > 1 and a_class[1] not in forbidden_class):
 
-            #exlude 'a' startswith '#' or 'https'
-            if not a.get('href').startswith('#') and not a.get('href').startswith('https') and not a.get('href').startswith('http') and not ":" in a.get('href'):
-                link_not_wiki = a.get('href').replace("/wiki/",'')
-                # print("enelever :" + link_not_wiki)
+            #exlude links starting with '#' or 'https:'/'http:' or ':'
+            if (not a.get('href').startswith('#') and not ":" in a.get('href')):
+                link_not_wiki = a.get('href').replace("/wiki/",'')#remove the '/wiki/' before the link
                 links_from_div.append(link_not_wiki)
-                # print(a.get('href'))
 
     return links_from_div
 
@@ -40,32 +32,31 @@ def liste_liens(page) -> list:
 #save a dico into the file
 def svg_dico(dico, file) -> None:
 
-    #auto close syntax
-    with open(file, "a") as f:
+    with open(file, "a") as f:#auto close syntax, create if not exist or append 
 
-        #prendre l'indice 0 de la list des clef et valeurs
-        # file.write("{}:{}\n".format(list(dico.keys())[0], list(dico.values())[0]))#old
+        #if the dico exist
         if len(dico) > 0:
-            f.write(f"{list(dico.keys())[0]}:{list(dico.values())[0]}\n")
+            f.write(f"{list(dico.keys())[0]}:{list(dico.values())[0]}\n")#take index 0 of key/value, because it only exist on index 0
         else:
             f.write("Empty dictionary\n")
 
 #Question 3 
 #use the file to creation a new dico
 def chg_dico(file) -> dict:
-    newdico = {}
-    with open(file, 'r') as f:
+    newdico = {}#dico to return 
+    with open(file, 'r') as f:#only read 
+        #for each line, add it to dico if exist
         for line in f:
             line = line.strip()#remove escape
             if not line:
-                continue  # ignorer les lignes vides
-            key, value = line.split(':', 1)#split from : 1 time
+                continue  # ignore empty lines
+            key, value = line.split(':', 1)#split from ':' 1 time
             newdico[key] = value
     return newdico
 
 #Question 4
 def graph_path(): 
-    file_name = "f1.txt"
+    file_name = "f2.txt"
     links_visited = []
     links_to_visite = ['Petyr_Baelish']
 
@@ -77,17 +68,18 @@ def graph_path():
         if first_elem in links_visited: # we leave if it's already visited
             continue 
 
-        with open('test.txt','a') as f:
+        #test
+        # with open('test.txt','a') as f:
+        #     links_visited.append(first_elem)
+        #     f.write(f"first element {first_elem} \n")
+        #     f.write(f"to visite {links_to_visite} \n")
+        #     f.write(f"visited {links_visited}\n")
+        #     links = liste_liens(first_elem)
+        #     f.write(f"all : {first_elem}:{links}\n")
+        #     f.write("\n")
 
-            links_visited.append(first_elem)
-            f.write(f"first element {first_elem} \n")
-            f.write(f"to visite {links_to_visite} \n")
-            f.write(f"visited {links_visited}\n")
-            links = liste_liens(first_elem)
-            f.write(f"all : {first_elem}:{links}\n")
-            f.write("\n")
-        # links_visited.append(first_elem)
-        # links = liste_liens(first_elem)
+        links_visited.append(first_elem)
+        links = liste_liens(first_elem)
         svg_dico({first_elem:links},file_name) #write into the file
         for link in links:
             # if link in links_visited:
@@ -106,5 +98,5 @@ def graph_path():
 # testdico = chg_dico("f1.txt")
 #test Q4
 graph_path()
-# print(liste_liens("House_Frey"))# test
+
 
